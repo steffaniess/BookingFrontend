9f12057dbabde16a837b8f006ded40496889ddd4
Stefanie Schubert <stefanie.schubert@hotmail.com>
2024-05-03 15:15:18 +0200

    add comment to text body

--- bookingfrontend/src/App.js
+++ bookingfrontend/src/App.js
@@ -1,19 +1,12 @@
-import { BrowserRouter as Route } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Routing from './components/partials/Routing';
 import Footer from './components/navigation/footer/Footer';
-import Home from './components/views/home/Home';
-import About from './components/views/about/About';
-
 
 function App() {
   return (<>
 <Navigation />
-<Routing>
-  <Route path="/" exact component={Home} />
-  <Route path="/about" component={About} />
-</Routing>
+<Routing/>
 <Footer/>
 </>
   );
--- bookingfrontend/src/components/navigation/body/Body.jsx
+++ bookingfrontend/src/components/navigation/body/Body.jsx
@@ -1,39 +1,42 @@
-import React from "react";
+// Import React and CSS 
+import React from "react"; 
 import "./Body.css";
+
+// Import child components
 import Footer from "../footer/Footer";
 import Home from "../../views/home/Home";
 import About from "../../views/about/About";
 import Booking from "../../views/booking/Booking";
 import Contact from "../../views/contact/Contact";
 
+// Body component to render different page content  
 const Body = ({page}) => {
-    let content;
 
-    if(page  === 'home') {
-        content = <Home />;
+  // Determine which page content to render
+  let content;
 
-      } else if (page === 'about') {
-        content = <About />;
-      }
+  if(page  === 'home') {
+    content = <Home />;
 
-      else if (page === 'booking') {
-        content = <Booking />;
-      }
+  } else if (page === 'about') {
+    content = <About />;
+  
+  } else if (page === 'booking') {
+    content = <Booking />;
+  
+  } else if (page === 'contact') {
+    content = <Contact />;
+  }
 
-      else if (page === 'contact') {
-        content = <Contact />;
-      }
-
-    return (
-        <div className="home">
-         
-            <Body>
-                {content}
-            </Body>
-            <Footer />
-        </div>
-        
-    );
+  // Render page content and footer 
+  return (
+    <div className="home">   
+      <Body>
+        {content} 
+      </Body>
+      <Footer />
+    </div>
+  );
 }
 
 export default Body;
